00b65adabd4620d874207c772c98433c58eabadd
tonmoy <tonmoy3673@gmail.com>
2022-10-27 02:40:58 +0600

    load category data

--- index.js
+++ index.js
@@ -19,6 +19,19 @@ app.get('/categories',(req,res)=>{
     res.send(categories)
 })
 
+app.get('/course/:id',(req,res)=>{
+    const id=req.params.id;
+   const selectedCourse=course.find(n=>n._id===id);
+   res.send(selectedCourse);
+    
+})
+
+app.get('/category/:id',(req,res)=>{
+    const id=req.params.id;
+    const selectedCategory=course.filter(n=>n.category_id===id);
+    res.send(selectedCategory);
+})
+
 app.listen(port,()=>{
     console.log('Code Academy server running on post',port);
 })
