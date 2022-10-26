f79fc870886a9211923c7ed6a20516c1ead303fb
tonmoy <tonmoy3673@gmail.com>
2022-10-27 00:13:05 +0600

    loaded course data

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express=require('express')
 const app=express();
+const cors=require('cors')
 const port=process.env.PORT || 5000;
+app.use(cors());
+const course=require('./course.json');
 
 
 app.get('/',(req,res)=>{
     res.send('Code API Running');
 })
+
+
+app.get('/course',(req,res)=>{
+    res.send(course)
+})
+
+
+
 
 app.listen(port,()=>{
     console.log('Code Academy server running on post',port);
