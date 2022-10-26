1dc3d2464bd02edc52d0aa10b2d31df0193503f3
tonmoy <tonmoy3673@gmail.com>
2022-10-27 01:13:44 +0600

    set navlink

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors=require('cors')
 const port=process.env.PORT || 5000;
 app.use(cors());
 const course=require('./course.json');
-
+const categories=require('./categories.json')
 
 app.get('/',(req,res)=>{
     res.send('Code API Running');
@@ -15,8 +15,9 @@ app.get('/course',(req,res)=>{
     res.send(course)
 })
 
-
-
+app.get('/categories',(req,res)=>{
+    res.send(categories)
+})
 
 app.listen(port,()=>{
     console.log('Code Academy server running on post',port);
